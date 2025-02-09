d621ee83986dcc53177c648da1462e4818446c54
xlan <zhuiming.mu@gmail.com>
2025-02-09 13:27:30 +0800

    update

--- i18n/en.js
+++ i18n/en.js
@@ -1,15 +1,15 @@
 Object.assign((typeof window === 'undefined' ? module.exports : window).i18n = (typeof window === 'undefined' ? module.exports : window).i18n || {}, {
   en: {
-    TitleText: 'Connect on Delta Chat',
-    DescriptionText: 'Delta Chat is a messaging app that works over e-mail.',
-    FormBlurbText: 'Create a Delta Chat invite link',
-    StepCodeText: 'Open Delta Chat, then navigate to <b>QR code → Show QR Invite Code → Copy to Clipboard</b>',
+    TitleText: 'Connect on ForkStar',
+    DescriptionText: 'ForkStar is a messaging app that works over e-mail.',
+    FormBlurbText: 'Create a ForkStar invite link',
+    StepCodeText: 'Open ForkStar, then navigate to <b>QR code → Show QR Invite Code → Copy to Clipboard</b>',
     StepPasteText: 'Paste into the following field:',
     StepShareText: 'Share the generated link over any channel',
     ShareLinkText: 'Open link',
     JoinText: 'Say hello to',
     JoinGroupText: 'Join',
     ChatText: 'Open chat',
-    DownloadText: 'Download Delta Chat',
+    DownloadText: 'Download ForkStar',
   },
 });
